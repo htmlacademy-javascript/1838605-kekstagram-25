6a66422e5631489c511121e05e36acf80c5b959c
Marty <Marty2009@yandex.ru>
2022-04-05 00:05:21 +0300

    Добафил фильтрацию галереи по нажатию кнопок фильтров

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 //import {generatePhotos} from './data.js';
-import {renderPhotos} from './mini-picture.js';
+import {renderPhotos, setupGallery, setImgFilterButtonClick} from './mini-picture.js';
 import {enableValidation} from './upload-form.js';
 import {getDataFromServer} from './server-api.js';
 
-getDataFromServer(renderPhotos);
+getDataFromServer((photos) => {
+  renderPhotos(photos);
+  setupGallery();
+  setImgFilterButtonClick(() => renderPhotos(photos));
+});
 enableValidation();
