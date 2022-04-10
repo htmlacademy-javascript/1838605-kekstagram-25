1a75db13bb197812f83945a25f61b836ce5452f0
Marty <Marty2009@yandex.ru>
2022-04-10 10:23:16 +0300

    исправил ошибки по критериям Б5, Б12, Д15

--- js/util.js
+++ js/util.js
@@ -11,25 +11,6 @@ function getRandomInt(a, b) {
 }
 
 /**
- * Возвращает случайный элемент из массива
- * @param {any[]} array - массив данных
- * @returns {any} случайный элемент массива
- */
-function getRandomArrayElement(array) {
-  return array[getRandomInt(0, array.length - 1)];
-}
-
-/**
- * Проверяет максимальную длину строки.
- * @param {string} comment - строка для проверки
- * @param {number} length - длина, с которой надо сравнить длину строки comment
- * @returns {boolean} true, если длина строки не больше
- */
-function checkStringLength(comment, length = 100) {
-  return comment.length <= length;
-}
-
-/**
  * Проверяет, что нажатая клавиша соответствует клавише Escape
  * @param {object} evt
  * @returns {boolean} true, если Escape
@@ -44,13 +25,12 @@ function isEscapeKey(evt) {
  * @returns {boolean}
  */
 function isExistSameElement(array) {
-  const temp = {};
+  const tempArray = {};
   for (let i = 0; i < array.length; i++) {
-    if (temp[array[i]]) {
+    if (tempArray[array[i]]) {
       return true;
-    } else {
-      temp[array[i]] = 1;
     }
+    tempArray[array[i]] = 1;
   }
   return false;
 }
@@ -79,4 +59,4 @@ function debounce(callback, timeoutDelay = 500) {
   };
 }
 
-export {getRandomInt, getRandomArrayElement, checkStringLength, isEscapeKey, isExistSameElement, debounce};
+export {getRandomInt, isEscapeKey, isExistSameElement, debounce};
